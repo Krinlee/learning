fc901fbe22f76a9dcdc3f14422e37eb4780feeb2
Krinlee <mrkrinlee@gmail.com>
2024-04-17 15:55:32 -0400

    updates

--- VoiceAssistant/main.py
+++ VoiceAssistant/main.py
@@ -8,7 +8,32 @@ def speak(text):
     tts.save(filename)
     playsound.playsound(filename)
 
-speak("death awaits you all with big nasty pointy teeth")
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+
+        except Exception as e:
+            print("Exception" + str(e))
+
+    return said
+
+
+text = get_audio()
+
+if "hello" in text:
+    speak("hello, how are you?")
+
+if "what is your name" in text:
+    speak("Some call me Tim")
+
+# speak("death awaits you all with big nasty pointy teeth")
+# get_audio()
 
 
 
