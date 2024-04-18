9f2122a292976a2ea5b39f643c14e6f0fdf5560b
Krinlee <mrkrinlee@gmail.com>
2024-04-17 20:47:03 -0400

    update

--- VoiceAssistant/main.py
+++ VoiceAssistant/main.py
@@ -3,7 +3,7 @@ import speech_recognition as sr
 from gtts import gTTS
 
 def speak(text):
-    tts = gTTS(text=text, lang="en")
+    tts = gTTS(text=text, lang="en", tld='co.uk')
     filename = "voice.mp3"
     tts.save(filename)
     playsound.playsound(filename)
@@ -24,15 +24,18 @@ def get_audio():
     return said
 
 
-text = get_audio()
+# text = get_audio()
 
-if "hello" in text:
-    speak("hello, how are you?")
+# if "hello" in text:
+#     speak("hello, how are you?")
 
-if "what is your name" in text:
-    speak("Some call me Tim")
+# if "what is your name" in text:
+#     speak("Some call me Tim")
 
-# speak("death awaits you all with big nasty pointy teeth")
+# if "why" in text:
+#     speak("you know! what you did!")
+
+speak("death awaits you all! with big nasty pointy teeth.")
 # get_audio()
 
 
