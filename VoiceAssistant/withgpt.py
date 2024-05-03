3004da960e2a7c9b951e9354b822051eae63b571
Krinlee <mrkrinlee@gmail.com>
2024-05-03 09:28:15 -0400

    updates

--- VoiceAssistant/withgpt.py
+++ VoiceAssistant/withgpt.py
@@ -6,48 +6,49 @@ from gtts import gTTS
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_KEY')
 
-is_looping = True
 
-# while is_looping == True:
+while True:
 
-def speak(text):
-    tts = gTTS(text=text, lang="en", tld='com.au')
-    filename = "voice.mp3"
-    tts.save(filename)
-    playsound.playsound(filename)
+    convo_memory = []
+    def speak(text):
+        tts = gTTS(text=text, lang="en", tld='com.au')
+        filename = "voice.mp3"
+        tts.save(filename)
+        playsound.playsound(filename)
 
-def get_audio():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        audio = r.listen(source)
-        said = ""
+    def get_audio():
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+            said = ""
 
-        try:
-            said = r.recognize_google(audio)
-            print(said)
+            try:
+                said = r.recognize_google(audio)
+                print(said)
 
-        except Exception as e:
-            print("Exception" + str(e))
+            except Exception as e:
+                print("Exception" + str(e))
 
-    return said
+        return said
 
-def get_response():
-    completion = openai.ChatCompletion.create(model = "gpt-3.5-turbo", messages = [
-            {"role": "system", "content": "Your are a nice person who enjoys having conversation."},
-            {"role": "user", "content": text}],
-            temperature = 0.1, max_tokens = 1000)
+    def get_response():
+        completion = openai.ChatCompletion.create(model = "gpt-3.5-turbo", messages = [
+                {"role": "system", "content": "Your are a nice person who enjoys having conversation. "},
+                {"role": "user", "content": text}],
+                temperature = 0.1, max_tokens = 1000)
 
-    response = completion.choices[0].message.content
-    return response
+        response = completion.choices[0].message.content
+        convo_memory.append({"role": "user", "content": text})
+        convo_memory.append({"role": "user", "content": response})
+        return response
 
-text = get_audio()
+    text = get_audio()
+    reply = get_response()
 
-reply = get_response()
-
-    # if "i'm done" in text:
-    #     break
-    # else:
-speak(reply)
+    if text.lower() == "i'm done":
+        break
+    else:
+        speak(reply)
 
 
 
